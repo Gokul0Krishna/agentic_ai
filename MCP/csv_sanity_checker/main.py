b1e55d30dff3db29de8db4550b4565c642fb009f
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2025-09-18 20:43:03 +0530

    why is

---
+++ MCP/csv_sanity_checker/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from csv-sanity-checker!")
+
+
+if __name__ == "__main__":
+    main()
--- MCP/csv_sanity_checker/test_1.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv(r"C:\Users\ASUS\OneDrive\Desktop\code\datasets\archive\ethusd_1min_ohlc.csv")
-print(df[:100])
