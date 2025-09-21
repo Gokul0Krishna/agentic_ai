30a3673a29a02b840788e940d5135740df8da9e2
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2025-09-21 10:13:36 +0530

    woah this works for now

--- MCP/csv_sanity_checker/sever.py
+++ MCP/csv_sanity_checker/sever.py
@@ -3,11 +3,18 @@ import pandas as pd
 
 mcp = FastMCP("Kill")
 
-@mcp.tool
-def Null_checker(df:pd.DataFrame):
-    'checks for null values in a dataframe'
-    null_count = df.isnull().sum()
-    if null_count.sum()>0:
-        return null_count.abs()
-    else:
-        return False
+# @mcp.tool
+# def Null_checker(df:pd.DataFrame):
+#     'checks for null values in a dataframe'
+#     null_count = df.isnull().sum()
+#     if null_count.sum()>0:
+#         return null_count.abs()
+#     else:
+#         return False
+
+@mcp.tool()
+def ReturnHello():
+    return 'hello'
+
+if __name__ == "__main__":
+    mcp.run()
